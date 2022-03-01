fc4ef991b671cadf3d027536e5c73be98c1e5e50
bilalozair <bo569@nyu.edu>
2022-03-01 21:13:05 +0500

    commit tree 4965223401b66084af501821173e10ec759e5db7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+saturdayFun();
+
+function saturdayFun(activity) {
+    let string1 ='This Saturday, I want to roller-skate!';
+    let string2 = 'This Saturday, I want to ';
+    return (activity === undefined) ? (string1) : (string2 + activity + "!");
+}
+
+const mondayWork = function(activity) {
+    let string3 ='This Monday, I will go to the office.';
+    let string4 = 'This Monday, I will '
+    return (activity === undefined) ? (string3) : (string4 + activity +".");
+}
+function wrapAdjective(string) {
+    const innerFunction = function (inner = "special") {
+      return `You are ${string}${inner}${string}!`;
+    };
+    return innerFunction;
+  }
